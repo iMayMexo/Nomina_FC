d1a2c48fe09960caf3af154cd092507883a6e019
Carlos <desarrollo@america-carrental.com>
2021-05-18 23:04:38 -0500

    setup

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+      <div class="container text-center">
+          <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h1>N&oacute;mina Resuelve FC</h1>
+              </div>
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            </div>
+              <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+              </div>
+          </div>
+          <br>
+          <div class="row">
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <strong>By Carlos M. G&oacute;mez</strong>
+              </div>
+          </div>
+      </div>
+
+      <!--script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script-->
+      <link rel="stylesheet" href="bootstrap.min.css">
+      <link rel="stylesheet" href="style.css">
+
+      <script type="text/javascript" src="bootstrap.min.js"></script>
+      <script type="text/javascript" src="js.js"></script>
+  </body>
+</html>
